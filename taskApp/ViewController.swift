fe18d0a60949e83d308fcd8f75915f14cb5c6cae
masayoshi.suzuki <sp1103masa@gmail.com>
2020-10-01 12:39:43 +0900

    Initial Commit

---
+++ taskApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskApp
+//
+//  Created by 鈴木正義 on 2020/10/01.
+//  Copyright © 2020 masayoshi.suzuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
