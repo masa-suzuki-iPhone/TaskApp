acd87c723d8a7725c5f38b835f8a55149f9fd4cb
masayoshi.suzuki <sp1103masa@gmail.com>
2020-10-04 10:46:45 +0900

    pod install と.gitignoreまでの実態

---
+++ taskApp/InputViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InputViewController.swift
+//  taskApp
+//
+//  Created by 鈴木正義 on 2020/10/02.
+//  Copyright © 2020 masayoshi.suzuki. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    @IBOutlet weak var titleTextView: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var dataPicker: UIDatePicker!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- taskApp/ViewController.swift
+++ taskApp/ViewController.swift
@@ -8,13 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    // データの数（セルの数を返すメソッド）
+    func tableView ( _ tableView: UITableView, numberOfRowsInSection section : Int ) -> Int {
+        return 0
+    }
+    
+    //各セルの内容を返すメソッド
+    func tableView ( _ tableView : UITableView, cellForRowAt indexPath : IndexPath) -> UITableViewCell {
+        // 再利用可能なCellを得る
+        let cell = tableView . dequeueReusableCell(withIdentifier: "Cell", for : indexPath )
+        return cell
     }
 
+     // 各セルを選択した時に実行されるメソッド
+    func tableView ( _ tableView : UITableView, didSelectRowAt IndexPath : IndexPath ) {
+        performSegue(withIdentifier: "cellSegue", sender: nil) //↑　＋ボタンをタップした時に移動できるようになった
+    }
+    
+    // セルが消去可能なことを伝えるメソッド
+    func tableView ( _ tableView : UITableView, editingStyleForRowAt indexPath : IndexPath)-> UITableViewCell . EditingStyle {
+        return . delete
+    }
+    
+    // Delete　ボタンが押された時に呼ばれるメソッド
+    func tableView ( _ tableView : UITableView, commit editingStyle : UITableViewCell . EditingStyle , forRowAt indexPath : IndexPath) {
+    
+    }
 
 }
 
